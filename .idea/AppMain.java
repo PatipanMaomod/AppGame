8cfa1cad56c3c22df4a2faca018b9b005536db28
Soramint <lukyat.2010@gmail.com>
2024-08-30 23:34:28 +0700

    add kiki

--- .idea/AppMain.java
+++ .idea/AppMain.java
@@ -2,4 +2,10 @@ class AppMain {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+}
+class kiki {
+    public static void main(String[] args) {
+
+    }
 }
