087c37ad18866221b563071aef9346723e287fa1
patipan <patipanmaomod@gmail.com>
2024-08-30 23:39:41 +0700

    555

--- .idea/AppMain.java
+++ .idea/AppMain.java
@@ -1,5 +1,10 @@
 class AppMain {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        if (1 < 5  ) {
+            System.out.println("EIEi");
+        }
+
     }
 }
